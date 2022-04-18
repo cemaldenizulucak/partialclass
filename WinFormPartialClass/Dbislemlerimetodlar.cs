36d5b3e85859a7ddbc2368ece256f2a8f9a00f9e
cemaldenizulucak <39682041+cemaldenizulucak@users.noreply.github.com>
2022-04-18 16:05:44 +0300

    partialclass
    Partial Class Kullanımları

---
+++ WinFormPartialClass/Dbislemlerimetodlar.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormPartialClass
+{
+    public partial class Dbislemler
+    {
+        public void InsertUrunler()
+        {
+                
+        }
+
+        public void InsertKategoriler()
+        {
+
+        }
+
+    }
+}
---
+++ WinFormPartialClass/Dbislemlertanımlamalar.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormPartialClass
+{
+    partial class Dbislemler
+    {
+        public int kdvoranı = 18;
+        public string kargocu = "Getir Kargo";
+    }
+}
